2ad32d486478fc321c25ed04604663c4141d4908
Rajagopalan Subrahmanian <rsubrahmania@juniper.net>
2022-05-09 16:25:54 -0400

    add telemetry support

---
+++ scripts/telemetry/start_telemetry.py
@@ -0,0 +1,27 @@
+import aos
+from aos.client import AosClient
+import urllib3
+import json
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+# You will need to update the connection details below with your
+# specific AOS instance
+AOS_IP = "66.129.234.206"
+AOS_PORT = 37000 
+AOS_USER = "admin"
+AOS_PW = "admin"
+
+# Where is the listener listening?
+LISTENER_IP = "100.123.0.8"
+ALERTS_PORT = 64429
+EVENTS_PORT = 64428
+PERFMON_PORT = 64427
+
+# Login
+aos = AosClient(protocol="https", host=AOS_IP, port=AOS_PORT)
+aos.auth.login(AOS_USER, AOS_PW)
+aos.telemetry_mgr.add_endpoint("100.123.0.8", ALERTS_PORT, "alerts")
+aos.telemetry_mgr.add_endpoint("100.123.0.8", EVENTS_PORT, "events")
+aos.telemetry_mgr.add_endpoint("100.123.0.8", PERFMON_PORT, "perfmon")
+print(aos.telemetry_mgr.get_endpoints())
